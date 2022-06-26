4ac95a6ad6801e59e3985e5fe0181db55fb9726f
Nicolas DECRESSAC <decressac.nicolas@gmail.com>
2022-06-26 15:50:01 +0200

    Update article animation
    updating script button.js
    fix some margin & padding for footer
    adding an min-height main for footer

--- public/js/button.js
+++ public/js/button.js
@@ -1,7 +1,10 @@
-// Init translate value
+// Init width value
 
-let elementTransform;
-elementTransform = "translateX(200%)";
+let elementTranslate;
+elementTranslate = "translateX(100%)";
+
+let elementMaxHeight;
+elementMaxHeight = "0";
 
 // get Buttons from DOM  and defined action on click for each
 
@@ -12,48 +15,86 @@ let softSkillsButton = document.querySelector("#softSkillsCard");
 let interestButton = document.querySelector("#interestCard");
 
 function trainingTranslate() {
-  trainingButton.style.transform = `${elementTransform}`;
-  if (elementTransform === "translateX(200%)") {
+  trainingButton.style.transform = `${elementTranslate}`;
+  if (`${elementTranslate}` === "translateX(100%)") {
     trainingButton.style.transition = "all 500ms ease-out";
-    elementTransform = trainingButton.style.transform = "translateX(0%)";
-  } else {
-    elementTransform = trainingButton.style.transform = "translateX(200%)";
+    trainingButton.style.transform = "translateX(0)";
+    trainingButton.style.maxHeight = "10000px";
+    elementTranslate = "translateX(0)";
+    elementMaxHeight = "fit-content";
+  } else if (`${elementTranslate}` === "translateX(0)") {
+    trainingButton.style.transition = "all 500ms ease-out";
+    trainingButton.style.transform = "translateX(100%)";
+    trainingButton.style.maxHeight = "0";
+    elementTranslate = "translateX(100%)";
+    elementMaxHeight = "0";
   }
 }
 
 function experiencesTranslate() {
-  experienceButton.style.transform = `${elementTransform}`;
-  if (elementTransform === "translateX(200%)") {
+  experienceButton.style.transform = `${elementTranslate}`;
+  if (`${elementTranslate}` === "translateX(100%)") {
     experienceButton.style.transition = "all 500ms ease-out";
-    elementTransform = experienceButton.style.transform = "translateX(0%)";
-  } else {
-    elementTransform = experienceButton.style.transform = "translateX(200%)";
+    experienceButton.style.transform = "translateX(0)";
+    experienceButton.style.maxHeight = "10000px";
+    elementTranslate = "translateX(0)";
+    elementMaxHeight = "fit-content";
+  } else if (`${elementTranslate}` === "translateX(0)") {
+    experienceButton.style.transition = "all 500ms ease-out";
+    experienceButton.style.transform = "translateX(100%)";
+    experienceButton.style.maxHeight = "0";
+    elementTranslate = "translateX(100%)";
+    elementMaxHeight = "0";
   }
 }
+
 function hardSkillsTranslate() {
-  hardSkillsButton.style.transform = `${elementTransform}`;
-  if (elementTransform === "translateX(200%)") {
+  hardSkillsButton.style.transform = `${elementTranslate}`;
+  if (`${elementTranslate}` === "translateX(100%)") {
     hardSkillsButton.style.transition = "all 500ms ease-out";
-    elementTransform = hardSkillsButton.style.transform = "translateX(0%)";
-  } else {
-    elementTransform = hardSkillsButton.style.transform = "translateX(200%)";
+    hardSkillsButton.style.transform = "translateX(0)";
+    hardSkillsButton.style.maxHeight = "10000px";
+    elementTranslate = "translateX(0)";
+    elementMaxHeight = "fit-content";
+  } else if (`${elementTranslate}` === "translateX(0)") {
+    hardSkillsButton.style.transition = "all 500ms ease-out";
+    hardSkillsButton.style.transform = "translateX(100%)";
+    hardSkillsButton.style.maxHeight = "0";
+    elementTranslate = "translateX(100%)";
+    elementMaxHeight = "0";
   }
 }
+
 function softSkillsTranslate() {
-  softSkillsButton.style.transform = `${elementTransform}`;
-  if (elementTransform === "translateX(200%)") {
+  softSkillsButton.style.transform = `${elementTranslate}`;
+  if (`${elementTranslate}` === "translateX(100%)") {
     softSkillsButton.style.transition = "all 500ms ease-out";
-    elementTransform = softSkillsButton.style.transform = "translateX(0%)";
-  } else {
-    elementTransform = softSkillsButton.style.transform = "translateX(200%)";
+    softSkillsButton.style.transform = "translateX(0)";
+    softSkillsButton.style.maxHeight = "10000px";
+    elementTranslate = "translateX(0)";
+    elementMaxHeight = "fit-content";
+  } else if (`${elementTranslate}` === "translateX(0)") {
+    softSkillsButton.style.transition = "all 500ms ease-out";
+    softSkillsButton.style.transform = "translateX(100%)";
+    softSkillsButton.style.maxHeight = "0";
+    elementTranslate = "translateX(100%)";
+    elementMaxHeight = "0";
   }
 }
+
 function interestTranslate() {
-  interestButton.style.transform = `${elementTransform}`;
-  if (elementTransform === "translateX(200%)") {
+  interestButton.style.transform = `${elementTranslate}`;
+  if (`${elementTranslate}` === "translateX(100%)") {
     interestButton.style.transition = "all 500ms ease-out";
-    elementTransform = interestButton.style.transform = "translateX(0%)";
-  } else {
-    elementTransform = interestButton.style.transform = "translateX(200%)";
+    interestButton.style.transform = "translateX(0)";
+    interestButton.style.maxHeight = "10000px";
+    elementTranslate = "translateX(0)";
+    elementMaxHeight = "fit-content";
+  } else if (`${elementTranslate}` === "translateX(0)") {
+    interestButton.style.transition = "all 500ms ease-out";
+    interestButton.style.transform = "translateX(100%)";
+    interestButton.style.maxHeight = "0";
+    elementTranslate = "translateX(100%)";
+    elementMaxHeight = "0";
   }
 }
